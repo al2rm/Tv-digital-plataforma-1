e87f69729638b0eb2289faed49de085a414b00e3
al2rm <44150179+al2rm@users.noreply.github.com>
2026-07-07 17:26:32 -0300

    Add files via upload

---
+++ backend/src/database/db.js
@@ -0,0 +1,17 @@
+import pg from "pg";
+import { env } from "../config/env.js";
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+  host: env.db.host,
+  port: env.db.port,
+  database: env.db.database,
+  user: env.db.user,
+  password: env.db.password
+});
+
+export const testConnection = async () => {
+  const result = await pool.query("SELECT NOW()");
+  return result.rows[0];
+};
